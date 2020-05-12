0d1c59a797ba7cc0a92265c7da4725cbad5258de
ravivats <vats.ravi96@gmail.com>
2020-05-13 01:50:36 +0800

    Add Day 12 singleNonDuplicate element O(n) solution

---
+++ platform-problems/leetcode/may-2020-leetcoding-challenge/12-LeetCode-singleNonDuplicate.py
@@ -0,0 +1,31 @@
+'''
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.
+
+Example 1:
+Input: [1,1,2,3,3,4,4,8,8], Output: 2
+
+Example 2:
+Input: [3,3,7,7,10,11,11], Output: 10
+ 
+Note: Your solution should run in O(log n) time and O(1) space.
+'''
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        elif len(nums) == 1:
+            return nums[0]
+            
+        i=1
+        while i < len(nums):
+            if nums[i-1] == nums[i]:
+                i = i+2
+            else:
+                return nums[i-1]
+            
+        return nums[-1] # return last element
+        
