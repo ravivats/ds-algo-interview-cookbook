e070bd17d84c799a8676c871d90989abd38f27d2
ravivats <vats.ravi96@gmail.com>
2020-07-07 01:57:05 +0800

    [feat] Add OddEvenList solution

---
+++ platform-problems/leetcode/may-2020-leetcoding-challenge/leetcode_16_odd_even_list/ListNode.java
@@ -0,0 +1,21 @@
+package leetcode_16_odd_even_list;
+
+/**
+ * Definition for singly-linked list.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ platform-problems/leetcode/may-2020-leetcoding-challenge/leetcode_16_odd_even_list/Solution.java
@@ -0,0 +1,20 @@
+package leetcode_16_odd_even_list;
+
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode originalEvenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = originalEvenHead;
+        return head;
+    }
+}
