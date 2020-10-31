5d0af8446004100610c44a137bb5adabb2993c3e
ravivats <vats.ravi96@gmail.com>
2020-11-01 00:17:33 +0800

    [feat] Add diff way to solve two sum

--- platform-problems/leetcode/leetcode-problems/P001_Two_Sum.java
+++ platform-problems/leetcode/leetcode-problems/P001_Two_Sum.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Given an array of integers nums and an integer target, return indices of the
@@ -17,12 +19,12 @@ import java.util.Arrays;
  */
 
 public class P001_Two_Sum {
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSumReturnNumbers(int[] nums, int target) {
         Arrays.sort(nums);
         int low = 0, high = nums.length - 1;
         while (low < high) {
             if (nums[low] + nums[high] == target)
-                return new int[] { low, high };
+                return new int[] { nums[low], nums[high] };
             else if (nums[low] + nums[high] > target)
                 high--;
             else
@@ -31,10 +33,23 @@ public class P001_Two_Sum {
         return new int[] { 0, 0 };
     }
 
+    public static int[] twoSumReturnIndices(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            Integer index = map.get(diff);
+            if (index != null)
+                return new int[] { i, index };
+            map.put(nums[i], i);
+        }
+        return new int[] { 0, 0 };
+    }
+
     public static void main(String[] args) {
-        int[] result = twoSum(new int[] { 2, 11, 7, 15 }, 9);
-        for (int i : result) {
-            System.out.println(i);
+        int[] numbers = twoSumReturnNumbers(new int[] { 2, 11, 7, 15 }, 9);
+        int[] indices = twoSumReturnIndices(new int[] { 2, 11, 7, 15 }, 9);
+        for (int i =0 ; i<2; i++){
+            System.out.printf("Num: %d, Index: %d\n", numbers[i], indices[i]);
         }
     }
 }
